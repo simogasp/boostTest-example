b34a0a4fd76e69704ac295587dd3c01ec1208f92
Simone Gasparini <simone.gasparini@gmail.com>
2017-07-28 19:56:32 +0200

    [computing] using BOOST_CHECK_CLOSE_FRACTION for back-compatibility

--- src/library/computing/test/testMultiplication.cpp
+++ src/library/computing/test/testMultiplication.cpp
@@ -24,7 +24,7 @@ BOOST_AUTO_TEST_SUITE( test_computeInterest )
     BOOST_AUTO_TEST_CASE( test_simple )
     {
         namespace tt = boost::test_tools;
-        BOOST_TEST(computeInterest(154.5, 1.15) == 177.675, tt::tolerance(0.01));
+        BOOST_CHECK_CLOSE_FRACTION(computeInterest(154.5, 1.15), 177.675, 0.01);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
