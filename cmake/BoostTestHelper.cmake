26af5658bf0a343eb00817b2e9ae757b5a5d92a6
Simone Gasparini <simone.gasparini@gmail.com>
2017-07-28 18:26:48 +0200

    [cmake] improved helper function

--- cmake/BoostTestHelper.cmake
+++ cmake/BoostTestHelper.cmake
@@ -1,15 +1,51 @@
-function(add_boost_test SOURCE_FILE_NAME DEPENDENCY_LIBS)
+#
+# Create a executable test from a source file. The associate executable has the same
+# name as the source file.
+#
+# Usage:
+#   add_boost_test(SOURCE <source>
+#                [INCLUDE [items1...] ]
+#                [LINK [items1...]
+#                [PREFIX <string>])
+#
+# SOURCE is the source file contaning the test
+# INCLUDE is an optional list of include directories
+# LINK is an optional list of targets/libraries to link with
+# PREFIX is an optional string to append to the name of the test (e.g. the module name)
+#     For example if foo.cpp is passed as source, and "bar" as PREFIX the executable
+#     will be named named "bar_foo". Without PREFIX it will be just "foo"
+function(add_boost_test)
+
+    set(options OPTIONS)
+    set(oneValueArgs SOURCE PREFIX)
+    set(multiValueArgs INCLUDE LINK)
+    cmake_parse_arguments(param "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+#    message(STATUS "INCLUDE ${param_INCLUDE}")
+#    message(STATUS "LINK ${param_LINK}")
+#    message(STATUS "PREFIX ${param_PREFIX}")
+#    message(STATUS "SOURCE ${param_SOURCE}")
 
     # get the name without the extension (NAME_WE)
-    get_filename_component(TEST_EXECUTABLE_NAME ${SOURCE_FILE_NAME} NAME_WE)
+    get_filename_component(TEST_EXECUTABLE_NAME ${param_SOURCE} NAME_WE)
 
-    add_executable(${TEST_EXECUTABLE_NAME} ${SOURCE_FILE_NAME})
+    if(NOT ${param_PREFIX} STREQUAL "")
+        set(TEST_EXECUTABLE_NAME "${param_PREFIX}_${TEST_EXECUTABLE_NAME}")
+#    else()
+#        message(STATUS "PREFIX not given")
+    endif()
+
+    add_executable(${TEST_EXECUTABLE_NAME} ${param_SOURCE})
+
+    if(NOT ${param_INCLUDE} STREQUAL "")
+        target_include_directories(${TEST_EXECUTABLE_NAME} ${param_INCLUDE})
+    endif()
 
     target_link_libraries(${TEST_EXECUTABLE_NAME}
-            ${DEPENDENCY_LIBS} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
+            ${param_LINK} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
 
     # test_case must be always contained in a test_suite
-    file(READ "${SOURCE_FILE_NAME}" SOURCE_FILE_CONTENTS)
+    file(READ "${param_SOURCE}" SOURCE_FILE_CONTENTS)
     string(REGEX MATCHALL "BOOST_AUTO_TEST_SUITE\\( *([A-Za-z_0-9]+) *\\)"
             FOUND_TESTS ${SOURCE_FILE_CONTENTS})
 
