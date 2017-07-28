acdd21ff05ce6ab3117cb21f7394325fe8736ec8
Simone Gasparini <simone.gasparini@gmail.com>
2017-07-28 20:19:41 +0200

    [doc]

--- src/library/computing/multiplication.hpp
+++ src/library/computing/multiplication.hpp
@@ -1,10 +1,23 @@
 #pragma once
 
+/**
+ * @brief Multiply two values.
+ * @tparam T the type of the values.
+ * @param first value
+ * @param second value
+ * @return the product
+ */
 template<typename T>
 T multiply(const T& first, const T& second)
 {
     return first * second;
 }
 
+/**
+ * @brief Compute the interests given a total and a interest ratio.
+ * @param total The total amount.
+ * @param ratio The interest ratio.
+ * @return the The effective amount of the interest.
+ */
 double computeInterest(double total, double ratio);
 
